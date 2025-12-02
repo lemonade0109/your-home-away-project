02d9cc51d5b9df532a4709be2ba6bbabff8958d8
Jubril <jubriloyebamiji@gmail.com>
2025-12-02 05:19:47 -0800

    corrected a bug

--- db/prisma.ts
+++ db/prisma.ts
@@ -1,13 +1,20 @@
 import { PrismaClient } from "@prisma/client";
 
 const prismaClientSingleton = () => {
-	return new PrismaClient();
+  return new PrismaClient({
+    log: process.env.NODE_ENV === "development" ? ["error", "warn"] : ["error"],
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL,
+      },
+    },
+  });
 };
 
 type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
 
 const globalForPrisma = globalThis as unknown as {
-	prisma: PrismaClientSingleton | undefined;
+  prisma: PrismaClientSingleton | undefined;
 };
 
 const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
